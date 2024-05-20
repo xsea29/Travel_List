1682ebaa75b5fba227e7b575e910b9da9377adfc
SAGAR KUMAR <126503955+xsea29@users.noreply.github.com>
2024-05-20 18:43:26 +0530

    third commit

--- travel_list_1.0/src/App.js
+++ travel_list_1.0/src/App.js
@@ -15,17 +15,25 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
-  const [description, setDescription] = useState(5);
+  const [description, setDescription] = useState(1);
   const [quantity, setQuantity] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
+        value={description}
+        onChange={(e) => setDescription(Number(e.target.value))}
       >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
@@ -37,8 +45,8 @@ function Form() {
       <input
         type="text"
         placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
       />
       <button>Add</button>
     </form>
